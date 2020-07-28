e68d9695594437ddb2f48f5dc8a83a0131d6e335
unknown <chuan.khuna@gmail.com>
2020-07-28 10:17:06 +0700

    Add model idea (not working)

--- preprocess_lib/preprocess_timeseries.py
+++ preprocess_lib/preprocess_timeseries.py
@@ -34,3 +34,22 @@ def tensor_to_numpy(tensor_ds):
     Y = np.asarray(Y)
 
     return X, Y
+
+
+def windowing_arr_X_Y(series, window_size, output_size=1, shuffle_buffer=1000):
+    """windowing the input series to ML train-dataset
+        window_size: time step of input
+        output_size: time step of output/forcast/predicted
+
+        return: tensor flow object
+    """
+
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    windows = ds.window(window_size + output_size, shift=1, drop_remainder=True)
+    ds = windows.flat_map(lambda window: window.batch(window_size + output_size))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda window: (window[:-output_size], window[-output_size:]))
+    ds = np.array(list(ds.as_numpy_iterator()))
+    x = np.array(ds[:, 0].tolist())
+    y = np.array(ds[:, 1].tolist())
+    return x, y
